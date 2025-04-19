59bf100480158a8d5fcb3e0080d481261dbe132f
Jeff Huang <jeffishjeff@gmail.com>
2025-04-19 14:31:18 +0800

    debit & credit on exchange

--- src/interfaces/IExchange.sol
+++ src/interfaces/IExchange.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IExchange {
+import {IERC6909Claims} from "./IERC6909Claims.sol";
+
+interface IExchange is IERC6909Claims {
     function unlock(bytes calldata data) external returns (bytes memory result);
+
+    function debit(address user, uint256 chainId, address token, uint256 amount) external;
+    function credit(address user, uint256 chainId, address token, uint256 amount) external;
 }
