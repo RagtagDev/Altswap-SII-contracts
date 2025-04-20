eb1fad6857b69ddb0efc04c596cecb9b389d9b35
wangshouh <wongshouhao@outlook.com>
2025-04-20 20:14:00 +0800

    Add basic initiate function

--- src/Agent.sol
+++ src/Agent.sol
@@ -5,14 +5,16 @@ import {IL2ToL2CrossDomainMessenger} from "./interfaces/external/IL2ToL2CrossDom
 import {IAgent} from "./interfaces/IAgent.sol";
 import {IBroker} from "./interfaces/IBroker.sol";
 import {Predeploys} from "./libraries/external/Predeploys.sol";
-import {TokenData} from "./models/TokenData.sol";
+import {TokenData, TokenDataLibrary} from "./models/TokenData.sol";
 
 contract Agent is IAgent {
+    using TokenDataLibrary for TokenData[];
     struct MessageCache {
         address sender;
         TokenData[] debitBundle;
     }
 
+    uint256 public messageNonce;
     // TODO: set
     uint256 internal immutable hubChainId;
     // TODO: set
@@ -30,10 +32,39 @@ contract Agent is IAgent {
         TokenData[] calldata debitBundle,
         TokenData[] calldata creditBundle
     ) external payable returns (bytes32 messageId) {
-        // TODO:
         // 1. debitBundle.transfer(msg.sender, address(this))
+        debitBundle.transferIn(msg.sender);
         // 2. call broker.handleMessage on hubChainId through messenger and get messgeHash
+        assembly ("memory-safe") {
+            let m := sload(messageNonce.slot)
+            mstore(0x00, m)
+            mstore(0x20, chainid())
+
+            messageId := keccak256(0x00, 0x40)
+
+            sstore(messageNonce.slot, add(m, 1))
+        }
+
+        messageCaches[messageId] = MessageCache({
+            sender: msg.sender,
+            debitBundle: debitBundle
+        });
+
         // 3. cache msg.sender, and debitBundle under messageHash
+        bytes memory message;
+
+        assembly ("memory-safe") {
+            let ptr := mload(0x40)
+            let copylen := add(sub(calldatasize(), 4), 0x20)
+            message := ptr
+
+            mstore(ptr, copylen)
+            mstore(add(ptr, 0x20), messageId)
+            calldatacopy(add(ptr, 0x40), 0x04, copylen)
+            mstore(0x40, add(ptr, copylen))
+        }
+
+        messenger.sendMessage(hubChainId, address(broker), message);
     }
 
     function release(address recipient, TokenData[] calldata creditBundle) external {
