7a29b622a6c3f13bf5a12a5b4a88e2e657117cd6
wangshouh <wongshouhao@outlook.com>
2025-04-21 20:26:51 +0800

    Add transferOut in TokenData

--- src/models/TokenData.sol
+++ src/models/TokenData.sol
@@ -48,6 +48,28 @@ library TokenDataLibrary {
         }
     }
 
+    /// @dev Sends `amount` of ERC20 `token` from the current contract to `to`.
+    /// Reverts upon failure.
+    function safeTransfer(TokenData memory self, address to) internal {
+        address token = self.token;
+        uint256 amount = self.amount;
+
+        assembly ("memory-safe") {
+            mstore(0x14, to) // Store the `to` argument.
+            mstore(0x34, amount) // Store the `amount` argument.
+            mstore(0x00, 0xa9059cbb000000000000000000000000) // `transfer(address,uint256)`.
+            // Perform the transfer, reverting upon failure.
+            let success := call(gas(), token, 0, 0x10, 0x44, 0x00, 0x20)
+            if iszero(and(eq(mload(0x00), 1), success)) {
+                if iszero(lt(or(iszero(extcodesize(token)), returndatasize()), success)) {
+                    mstore(0x00, 0x90b8ec18) // `TransferFailed()`.
+                    revert(0x1c, 0x04)
+                }
+            }
+            mstore(0x34, 0) // Restore the part of the free memory pointer that was overwritten.
+        }
+    }
+
     function transferIn(TokenData[] memory self, address from) internal {
         uint256 sumETHAmount = 0;
         for (uint256 i = 0; i < self.length; i++) {
@@ -61,5 +83,16 @@ library TokenDataLibrary {
         }
 
         require(sumETHAmount == msg.value, ETHTransferFailed());
+    }
+
+    function transferOut(TokenData[] memory self, address to) internal {
+        for (uint256 i = 0; i < self.length; i++) {
+            TokenData memory data = self[i];
+            if (data.token == ADDRESS_ZERO) {
+                safeTransferETH(to, data.amount);
+            } else {
+                data.safeTransfer(to);
+            }
+        }
     }
 }
